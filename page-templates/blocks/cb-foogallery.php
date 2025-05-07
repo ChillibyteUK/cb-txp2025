0f4a38d9efaa21a1b4366d13d027f0398fdc77c0
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2025-05-07 13:37:50 +0100

    Update cb-foogallery.php

--- page-templates/blocks/cb-foogallery.php
+++ page-templates/blocks/cb-foogallery.php
@@ -26,6 +26,13 @@ $gallery_id = get_field( 'gallery_id' );
             <h3 class="h4">Upload Your Photos</h3>
             <?= do_shortcode( '[foogallery_upload id="' . esc_attr( $gallery_id ) . '"]' ); ?>
         </div>
+        <script>
+        document.addEventListener('DOMContentLoaded', function () {
+            if (typeof fg_feu_init === 'function') {
+                fg_feu_init();
+            }
+        });
+        </script>
         <?php
     }
     ?>
