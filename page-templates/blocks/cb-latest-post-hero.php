256569cc9f2b195d20694c095b75c339d3d4ec8e
ChillibyteUK <dan@chillibyte.co.uk>
2025-05-19 14:23:44 +0000

    latest hero

--- page-templates/blocks/cb-latest-post-hero.php
+++ page-templates/blocks/cb-latest-post-hero.php
@@ -17,22 +17,25 @@ $title = get_the_title( $latest_post_id );
 $excerpt = get_the_excerpt( $latest_post_id );
 ?>
 <section class="latest_post_hero">
-    <div class="latest_post_hero__image">
-        <img src="<?php echo esc_url( $image ); ?>" alt="<?php echo esc_attr( $title ); ?>">
-    </div>
-    <div class="latest_post_hero__link">
-        <a href="<?php echo esc_url( get_permalink( $latest_post_id ) ); ?>"><i class="fa-solid fa-play"></i></a>
-    </div>
-    <div class="latest_post_hero__content">
-        <div class="container">
-            <div class="row g-4">
-                <div class="col-sm-6">
-                    <h2 class="mb-0"><?php echo esc_html( $title ); ?></h2>
-                </div>
-                <div class="col-sm-6">
-                    <p><?php echo esc_html( $excerpt ); ?></p>
-                </div>
-            </div>
-        </div>
-    </div>
+	<a href="<?php echo esc_url( get_permalink( $latest_post_id ) ); ?>">
+		<div class="latest_post_hero__image">
+			<img src="<?php echo esc_url( $image ); ?>" alt="<?php echo esc_attr( $title ); ?>">
+		</div>
+		<div class="latest_post_hero__link">
+			<i class="fa-solid fa-play"></i>
+		</div>
+		<div class="latest_post_hero__content">
+			<div class="container">
+				<div class="row g-4">
+					<div class="col-sm-6">
+						<h2 class="mb-0"><?php echo esc_html( $title ); ?></h2>
+					</div>
+					<div class="col-sm-6">
+						<p><?php echo esc_html( $excerpt ); ?></p>
+						<p><strong>New Episodes Daily</strong> - Click for today's new podcast.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</a>
 </section>
